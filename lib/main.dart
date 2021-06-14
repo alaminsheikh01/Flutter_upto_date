a94ac73ab918bcc1f0421a2c7d16a4cdd9afb32a
alamin <alamin@gmail.com>
2021-06-14 14:02:36 +0600

    beautiful footer part

--- lib/main.dart
+++ lib/main.dart
@@ -15,28 +15,28 @@ class MyApp extends StatefulWidget {
 class _State extends State<MyApp> {
   String _value = '';
 
+  void _onClick(String value) => setState(() => _value = value);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Name here'),
       ),
-      drawer: Drawer(
-        child: Container(
-          padding: EdgeInsets.all(30),
-          child: Column(
-            children: [
-              Text("Hello Drawer"),
-              ElevatedButton(
-                onPressed: () => Navigator.pop(context),
-                child: Text(
-                  "Closed",
-                ),
-              )
-            ],
-          ),
+      persistentFooterButtons: [
+        IconButton(
+          icon: Icon(Icons.timer),
+          onPressed: () => _onClick("Button one"),
         ),
-      ),
+        IconButton(
+          icon: Icon(Icons.people),
+          onPressed: () => _onClick("Button two"),
+        ),
+        IconButton(
+          icon: Icon(Icons.map),
+          onPressed: () => _onClick("Button three"),
+        )
+      ],
       body: Container(
         padding: EdgeInsets.all(32.0),
         child: Column(
